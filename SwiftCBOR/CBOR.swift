7c97c08a8c016ab19c6e2fc8e1925360f043e86a
timds <timds_pub@icloud.com>
2017-02-14 14:34:26 +0900

    Update to Swift 3

--- SwiftCBOR/CBOR.swift
+++ SwiftCBOR/CBOR.swift
@@ -1,96 +1,97 @@
-public indirect enum CBOR : Equatable, Hashable,
-	NilLiteralConvertible, IntegerLiteralConvertible, StringLiteralConvertible,
-	ArrayLiteralConvertible, DictionaryLiteralConvertible, BooleanLiteralConvertible,
-	FloatLiteralConvertible {
-	case UnsignedInt(UInt)
-	case NegativeInt(UInt)
-	case ByteString([UInt8])
-	case UTF8String(String)
-	case Array([CBOR])
-	case Map([CBOR : CBOR])
-	case Tagged(UInt, CBOR)
-	case Simple(UInt8)
-	case Boolean(Bool)
-	case Null
-	case Undefined
-	case Half(Float32)
-	case Float(Float32)
-	case Double(Float64)
-	case Break
 
+
+public indirect enum CBOR : Equatable, Hashable,
+	ExpressibleByNilLiteral, ExpressibleByIntegerLiteral, ExpressibleByStringLiteral,
+	ExpressibleByArrayLiteral, ExpressibleByDictionaryLiteral, ExpressibleByBooleanLiteral,
+	ExpressibleByFloatLiteral {
+	case unsignedInt(UInt)
+	case negativeInt(UInt)
+	case byteString([UInt8])
+	case utf8String(String)
+	case array([CBOR])
+	case map([CBOR : CBOR])
+	case tagged(UInt8, CBOR)
+	case simple(UInt8)
+	case boolean(Bool)
+	case null
+	case undefined
+	case half(Float32)
+	case float(Float32)
+	case double(Float64)
+	case `break`
 	public var hashValue : Int {
 		switch self {
-		case let .UnsignedInt(l): return l.hashValue
-		case let .NegativeInt(l): return l.hashValue
-		case let .ByteString(l):  return Util.djb2Hash(l.map { Int($0) })
-		case let .UTF8String(l):  return l.hashValue
-		case let .Array(l):	      return Util.djb2Hash(l.map { $0.hashValue })
-		case let .Map(l):         return Util.djb2Hash(l.map { $0.hashValue &+ $1.hashValue })
-		case let .Tagged(t, l):   return t.hashValue &+ l.hashValue
-		case let .Simple(l):      return l.hashValue
-		case let .Boolean(l):     return l.hashValue
-		case Null:                return -1
-		case Undefined:           return -2
-		case let .Half(l):        return l.hashValue
-		case let .Float(l):       return l.hashValue
-		case let .Double(l):      return l.hashValue
-		case Break:               return Int.min
+		case let .unsignedInt(l): return l.hashValue
+		case let .negativeInt(l): return l.hashValue
+		case let .byteString(l):  return Util.djb2Hash(l.map { Int($0) })
+		case let .utf8String(l):  return l.hashValue
+		case let .array(l):	      return Util.djb2Hash(l.map { $0.hashValue })
+		case let .map(l):         return Util.djb2Hash(l.map { $0.hashValue &+ $1.hashValue })
+		case let .tagged(t, l):   return t.hashValue &+ l.hashValue
+		case let .simple(l):      return l.hashValue
+		case let .boolean(l):     return l.hashValue
+		case .null:                return -1
+		case .undefined:           return -2
+		case let .half(l):        return l.hashValue
+		case let .float(l):       return l.hashValue
+		case let .double(l):      return l.hashValue
+		case .break:               return Int.min
 		}
 	}
 	
 	public subscript(position: CBOR) -> CBOR? {
 		get {
 			switch (self, position) {
-			case (let .Array(l), let .UnsignedInt(i)): return l[Int(i)]
-			case (let .Map(l), let i): return l[i]
+			case (let .array(l), let .unsignedInt(i)): return l[Int(i)]
+			case (let .map(l), let i): return l[i]
 			default: return nil
 			}
 		}
 		set(x) {
 			switch (self, position) {
-			case (var .Array(l), let .UnsignedInt(i)): l[Int(i)] = x!
-			case (var .Map(l), let i): l[i] = x!
+			case (var .array(l), let .unsignedInt(i)): l[Int(i)] = x!
+			case (var .map(l), let i): l[i] = x!
 			default: break
 			}
 		}
 	}
 
-	public init(nilLiteral: ()) { self = .Null }
+	public init(nilLiteral: ()) { self = .null }
 	public init(integerLiteral value: Int) {
-		if value < 0 { self = .NegativeInt(UInt(-value) - 1) } else { self = .UnsignedInt(UInt(value)) }
+		if value < 0 { self = .negativeInt(UInt(-value) - 1) } else { self = .unsignedInt(UInt(value)) }
 	}
-	public init(extendedGraphemeClusterLiteral value: String) { self = .UTF8String(value) }
-	public init(unicodeScalarLiteral value: String) { self = .UTF8String(value) }
-	public init(stringLiteral value: String) { self = .UTF8String(value) }
-	public init(arrayLiteral elements: CBOR...) { self = .Array(elements) }
+	public init(extendedGraphemeClusterLiteral value: String) { self = .utf8String(value) }
+	public init(unicodeScalarLiteral value: String) { self = .utf8String(value) }
+	public init(stringLiteral value: String) { self = .utf8String(value) }
+	public init(arrayLiteral elements: CBOR...) { self = .array(elements) }
 	public init(dictionaryLiteral elements: (CBOR, CBOR)...) {
 		var result = [CBOR : CBOR]()
 		for (key, value) in elements {
 			result[key] = value
 		}
-		self = .Map(result)
+		self = .map(result)
 	}
-	public init(booleanLiteral value: Bool) { self = .Boolean(value) }
-	public init(floatLiteral value: Float32) { self = .Float(value) }
+	public init(booleanLiteral value: Bool) { self = .boolean(value) }
+	public init(floatLiteral value: Float32) { self = .float(value) }
 }
 
 public func ==(lhs: CBOR, rhs: CBOR) -> Bool {
 	switch (lhs, rhs) {
-	case (let .UnsignedInt(l), let .UnsignedInt(r)): return l == r
-	case (let .NegativeInt(l), let .NegativeInt(r)): return l == r
-	case (let .ByteString(l),  let .ByteString(r)):  return l == r
-	case (let .UTF8String(l),  let .UTF8String(r)):  return l == r
-	case (let .Array(l),       let .Array(r)):       return l == r
-	case (let .Map(l),         let .Map(r)):         return l == r
-	case (let .Tagged(tl, l),  let .Tagged(tr, r)):  return tl == tr && l == r
-	case (let .Simple(l),      let .Simple(r)):      return l == r
-	case (let .Boolean(l),     let .Boolean(r)):     return l == r
-	case (.Null,               .Null):               return true
-	case (.Undefined,          .Undefined):          return true
-	case (let .Half(l),        let .Half(r)):        return l == r
-	case (let .Float(l),       let .Float(r)):       return l == r
-	case (let .Double(l),      let .Double(r)):      return l == r
-	case (.Break,              .Break):              return true
+	case (let .unsignedInt(l), let .unsignedInt(r)): return l == r
+	case (let .negativeInt(l), let .negativeInt(r)): return l == r
+	case (let .byteString(l),  let .byteString(r)):  return l == r
+	case (let .utf8String(l),  let .utf8String(r)):  return l == r
+	case (let .array(l),       let .array(r)):       return l == r
+	case (let .map(l),         let .map(r)):         return l == r
+	case (let .tagged(tl, l),  let .tagged(tr, r)):  return tl == tr && l == r
+	case (let .simple(l),      let .simple(r)):      return l == r
+	case (let .boolean(l),     let .boolean(r)):     return l == r
+	case (.null,               .null):               return true
+	case (.undefined,          .undefined):          return true
+	case (let .half(l),        let .half(r)):        return l == r
+	case (let .float(l),       let .float(r)):       return l == r
+	case (let .double(l),      let .double(r)):      return l == r
+	case (.break,              .break):              return true
 	default:                                         return false
 	}
 }
--- SwiftCBOR/CBORDecoder.swift
+++ SwiftCBOR/CBORDecoder.swift
@@ -1,7 +1,7 @@
-public enum CBORError : ErrorType {
-	case UnfinishedSequence
-	case WrongTypeInsideSequence
-	case IncorrectUTF8String
+public enum CBORError : Error {
+	case unfinishedSequence
+	case wrongTypeInsideSequence
+	case incorrectUTF8String
 }
 
 public class CBORDecoder {
@@ -20,30 +20,30 @@ public class CBORDecoder {
 		istream = ArraySliceUInt8(slice: input[0..<input.count])
 	}
 
-	private func readUInt<T: UnsignedIntegerType>(n: Int) throws -> T {
-		return UnsafePointer<T>(Array(try istream.popBytes(n)).reverse()).memory
+	private func readUInt<T: UnsignedInteger>(_ n: Int) throws -> T {
+        return UnsafeRawPointer(Array(try istream.popBytes(n).reversed())).load(as: T.self)
 	}
 
-	private func readN(n: Int) throws -> [CBOR] {
-		return try (0..<n).map { _ in guard let r = try decodeItem() else { throw CBORError.UnfinishedSequence }; return r }
+	private func readN(_ n: Int) throws -> [CBOR] {
+		return try (0..<n).map { _ in guard let r = try decodeItem() else { throw CBORError.unfinishedSequence }; return r }
 	}
 
 	private func readUntilBreak() throws -> [CBOR] {
 		var result : [CBOR] = []
 		var cur = try decodeItem()
-		while (cur != CBOR.Break) {
-			guard let curr = cur else { throw CBORError.UnfinishedSequence }
+		while (cur != CBOR.break) {
+			guard let curr = cur else { throw CBORError.unfinishedSequence }
 			result.append(curr)
 			cur = try decodeItem()
 		}
 		return result
 	}
 
-	private func readNPairs(n: Int) throws -> [CBOR : CBOR] {
+	private func readNPairs(_ n: Int) throws -> [CBOR : CBOR] {
 		var result : [CBOR : CBOR] = [:]
 		for _ in (0..<n) {
-			guard let key  = try decodeItem() else { throw CBORError.UnfinishedSequence }
-			guard let val  = try decodeItem() else { throw CBORError.UnfinishedSequence }
+			guard let key  = try decodeItem() else { throw CBORError.unfinishedSequence }
+			guard let val  = try decodeItem() else { throw CBORError.unfinishedSequence }
 			result[key] = val
 		}
 		return result
@@ -53,91 +53,95 @@ public class CBORDecoder {
 		var result : [CBOR : CBOR] = [:]
 		var key = try decodeItem()
 		var val = try decodeItem()
-		while (key != CBOR.Break) {
-			guard let okey = key else { throw CBORError.UnfinishedSequence }
-			guard let oval = val else { throw CBORError.UnfinishedSequence }
+		while (key != CBOR.break) {
+			guard let okey = key else { throw CBORError.unfinishedSequence }
+			guard let oval = val else { throw CBORError.unfinishedSequence }
 			result[okey] = oval
-			do { key = try decodeItem() } catch CBORError.UnfinishedSequence { key = nil }
-			guard (key != CBOR.Break) else { break } // don't eat the val after the break!
-			do { val = try decodeItem() } catch CBORError.UnfinishedSequence { val = nil }
+			do { key = try decodeItem() } catch CBORError.unfinishedSequence { key = nil }
+			guard (key != CBOR.break) else { break } // don't eat the val after the break!
+			do { val = try decodeItem() } catch CBORError.unfinishedSequence { val = nil }
 		}
 		return result
 	}
 
 	public func decodeItem() throws -> CBOR? {
 		switch try istream.popByte() {
-		case let b where b <= 0x17: return CBOR.UnsignedInt(UInt(b))
-		case 0x18: return CBOR.UnsignedInt(UInt(try istream.popByte()))
-		case 0x19: return CBOR.UnsignedInt(UInt(try readUInt(2) as UInt16))
-		case 0x1a: return CBOR.UnsignedInt(UInt(try readUInt(4) as UInt32))
-		case 0x1b: return CBOR.UnsignedInt(UInt(try readUInt(8) as UInt64))
+		case let b where b <= 0x17: return CBOR.unsignedInt(UInt(b))
+		case 0x18: return CBOR.unsignedInt(UInt(try istream.popByte()))
+		case 0x19: return CBOR.unsignedInt(UInt(try readUInt(2) as UInt16))
+		case 0x1a: return CBOR.unsignedInt(UInt(try readUInt(4) as UInt32))
+		case 0x1b: return CBOR.unsignedInt(UInt(try readUInt(8) as UInt64))
 
-		case let b where 0x20 <= b && b <= 0x37: return CBOR.NegativeInt(UInt(b - 0x20))
-		case 0x38: return CBOR.NegativeInt(UInt(try istream.popByte()))
-		case 0x39: return CBOR.NegativeInt(UInt(try readUInt(2) as UInt16))
-		case 0x3a: return CBOR.NegativeInt(UInt(try readUInt(4) as UInt32))
-		case 0x3b: return CBOR.NegativeInt(UInt(try readUInt(8) as UInt64))
+		case let b where 0x20 <= b && b <= 0x37: return CBOR.negativeInt(UInt(b - 0x20))
+		case 0x38: return CBOR.negativeInt(UInt(try istream.popByte()))
+		case 0x39: return CBOR.negativeInt(UInt(try readUInt(2) as UInt16))
+		case 0x3a: return CBOR.negativeInt(UInt(try readUInt(4) as UInt32))
+		case 0x3b: return CBOR.negativeInt(UInt(try readUInt(8) as UInt64))
 
-		case let b where 0x40 <= b && b <= 0x57: return CBOR.ByteString(Array(try istream.popBytes(Int(b - 0x40))))
-		case 0x58: return CBOR.ByteString(Array(try istream.popBytes(Int(try istream.popByte()))))
-		case 0x59: return CBOR.ByteString(Array(try istream.popBytes(Int(try readUInt(2) as UInt16))))
-		case 0x5a: return CBOR.ByteString(Array(try istream.popBytes(Int(try readUInt(4) as UInt32))))
-		case 0x5b: return CBOR.ByteString(Array(try istream.popBytes(Int(try readUInt(8) as UInt64))))
-		case 0x5f: return CBOR.ByteString(try readUntilBreak().flatMap { x -> [UInt8] in guard case .ByteString(let r) = x else { throw CBORError.WrongTypeInsideSequence }; return r })
+		case let b where 0x40 <= b && b <= 0x57: return CBOR.byteString(Array(try istream.popBytes(Int(b - 0x40))))
+		case 0x58: return CBOR.byteString(Array(try istream.popBytes(Int(try istream.popByte()))))
+		case 0x59: return CBOR.byteString(Array(try istream.popBytes(Int(try readUInt(2) as UInt16))))
+		case 0x5a: return CBOR.byteString(Array(try istream.popBytes(Int(try readUInt(4) as UInt32))))
+		case 0x5b: return CBOR.byteString(Array(try istream.popBytes(Int(try readUInt(8) as UInt64))))
+		case 0x5f: return CBOR.byteString(try readUntilBreak().flatMap { x -> [UInt8] in guard case .byteString(let r) = x else { throw CBORError.wrongTypeInsideSequence }; return r })
 
-		case let b where 0x60 <= b && b <= 0x77: return CBOR.UTF8String(try Util.decodeUtf8(try istream.popBytes(Int(b - 0x60))))
-		case 0x78: return CBOR.UTF8String(try Util.decodeUtf8(try istream.popBytes(Int(try istream.popByte()))))
-		case 0x79: return CBOR.UTF8String(try Util.decodeUtf8(try istream.popBytes(Int(try readUInt(2) as UInt16))))
-		case 0x7a: return CBOR.UTF8String(try Util.decodeUtf8(try istream.popBytes(Int(try readUInt(4) as UInt32))))
-		case 0x7b: return CBOR.UTF8String(try Util.decodeUtf8(try istream.popBytes(Int(try readUInt(8) as UInt64))))
-		case 0x7f: return CBOR.UTF8String(try readUntilBreak().map { x -> String in guard case .UTF8String(let r) = x else { throw CBORError.WrongTypeInsideSequence }; return r }.joinWithSeparator(""))
+		case let b where 0x60 <= b && b <= 0x77: return CBOR.utf8String(try Util.decodeUtf8(try istream.popBytes(Int(b - 0x60))))
+		case 0x78: return CBOR.utf8String(try Util.decodeUtf8(try istream.popBytes(Int(try istream.popByte()))))
+		case 0x79: return CBOR.utf8String(try Util.decodeUtf8(try istream.popBytes(Int(try readUInt(2) as UInt16))))
+		case 0x7a: return CBOR.utf8String(try Util.decodeUtf8(try istream.popBytes(Int(try readUInt(4) as UInt32))))
+		case 0x7b: return CBOR.utf8String(try Util.decodeUtf8(try istream.popBytes(Int(try readUInt(8) as UInt64))))
+		case 0x7f: return CBOR.utf8String(try readUntilBreak().map { x -> String in guard case .utf8String(let r) = x else { throw CBORError.wrongTypeInsideSequence }; return r }.joined(separator: ""))
 
-		case let b where 0x80 <= b && b <= 0x97: return CBOR.Array(try readN(Int(b - 0x80)))
-		case 0x98: return CBOR.Array(try readN(Int(try istream.popByte())))
-		case 0x99: return CBOR.Array(try readN(Int(try readUInt(2) as UInt16)))
-		case 0x9a: return CBOR.Array(try readN(Int(try readUInt(4) as UInt32)))
-		case 0x9b: return CBOR.Array(try readN(Int(try readUInt(8) as UInt64)))
-		case 0x9f: return CBOR.Array(try readUntilBreak())
+		case let b where 0x80 <= b && b <= 0x97: return CBOR.array(try readN(Int(b - 0x80)))
+		case 0x98: return CBOR.array(try readN(Int(try istream.popByte())))
+		case 0x99: return CBOR.array(try readN(Int(try readUInt(2) as UInt16)))
+		case 0x9a: return CBOR.array(try readN(Int(try readUInt(4) as UInt32)))
+		case 0x9b: return CBOR.array(try readN(Int(try readUInt(8) as UInt64)))
+		case 0x9f: return CBOR.array(try readUntilBreak())
 
-		case let b where 0xa0 <= b && b <= 0xb7: return CBOR.Map(try readNPairs(Int(b - 0xa0)))
-		case 0xb8: return CBOR.Map(try readNPairs(Int(try istream.popByte())))
-		case 0xb9: return CBOR.Map(try readNPairs(Int(try readUInt(2) as UInt16)))
-		case 0xba: return CBOR.Map(try readNPairs(Int(try readUInt(4) as UInt32)))
-		case 0xbb: return CBOR.Map(try readNPairs(Int(try readUInt(8) as UInt64)))
-		case 0xbf: return CBOR.Map(try readPairsUntilBreak())
+		case let b where 0xa0 <= b && b <= 0xb7: return CBOR.map(try readNPairs(Int(b - 0xa0)))
+		case 0xb8: return CBOR.map(try readNPairs(Int(try istream.popByte())))
+		case 0xb9: return CBOR.map(try readNPairs(Int(try readUInt(2) as UInt16)))
+		case 0xba: return CBOR.map(try readNPairs(Int(try readUInt(4) as UInt32)))
+		case 0xbb: return CBOR.map(try readNPairs(Int(try readUInt(8) as UInt64)))
+		case 0xbf: return CBOR.map(try readPairsUntilBreak())
 
 		case let b where 0xc0 <= b && b <= 0xd7:
-			guard let item = try decodeItem() else { throw CBORError.UnfinishedSequence }
-			return CBOR.Tagged(UInt(b - 0xc0), item)
+			guard let item = try decodeItem() else { throw CBORError.unfinishedSequence }
+			return CBOR.tagged(UInt8(b - 0xc0), item)
 		case 0xd8:
-			let tag = UInt(try istream.popByte())
-			guard let item = try decodeItem() else { throw CBORError.UnfinishedSequence }
-			return CBOR.Tagged(tag, item)
+			let tag = UInt8(try istream.popByte())
+			guard let item = try decodeItem() else { throw CBORError.unfinishedSequence }
+			return CBOR.tagged(tag, item)
 		case 0xd9:
-			let tag = UInt(try readUInt(2) as UInt16)
-			guard let item = try decodeItem() else { throw CBORError.UnfinishedSequence }
-			return CBOR.Tagged(tag, item)
+			let tag = UInt8(try readUInt(2) as UInt16)
+			guard let item = try decodeItem() else { throw CBORError.unfinishedSequence }
+			return CBOR.tagged(tag, item)
 		case 0xda:
-			let tag = UInt(try readUInt(4) as UInt32)
-			guard let item = try decodeItem() else { throw CBORError.UnfinishedSequence }
-			return CBOR.Tagged(tag, item)
+			let tag = UInt8(try readUInt(4) as UInt32)
+			guard let item = try decodeItem() else { throw CBORError.unfinishedSequence }
+			return CBOR.tagged(tag, item)
 		case 0xdb:
-			let tag = UInt(try readUInt(8) as UInt64)
-			guard let item = try decodeItem() else { throw CBORError.UnfinishedSequence }
-			return CBOR.Tagged(tag, item)
+			let tag = UInt8(try readUInt(8) as UInt64)
+			guard let item = try decodeItem() else { throw CBORError.unfinishedSequence }
+			return CBOR.tagged(tag, item)
 
-		case let b where 0xe0 <= b && b <= 0xf3: return CBOR.Simple(b - 0xe0)
-		case 0xf4: return CBOR.Boolean(false)
-		case 0xf5: return CBOR.Boolean(true)
-		case 0xf6: return CBOR.Null
-		case 0xf7: return CBOR.Undefined
-		case 0xf8: return CBOR.Simple(try istream.popByte())
+		case let b where 0xe0 <= b && b <= 0xf3: return CBOR.simple(b - 0xe0)
+		case 0xf4: return CBOR.boolean(false)
+		case 0xf5: return CBOR.boolean(true)
+		case 0xf6: return CBOR.null
+		case 0xf7: return CBOR.undefined
+		case 0xf8: return CBOR.simple(try istream.popByte())
 
-		case 0xf9: return CBOR.Half(loadFromF16(UnsafePointer<UInt16>(Array(try istream.popBytes(2)).reverse())))
-		case 0xfa: return CBOR.Float(UnsafePointer<Float32>(Array(try istream.popBytes(4)).reverse()).memory)
-		case 0xfb: return CBOR.Double(UnsafePointer<Float64>(Array(try istream.popBytes(8)).reverse()).memory)
+		case 0xf9:
+            let ptr = UnsafeRawPointer(Array(try istream.popBytes(2).reversed())).bindMemory(to: UInt16.self, capacity: 1)
+            return CBOR.half(loadFromF16(ptr))
+		case 0xfa:
+            return CBOR.float(UnsafeRawPointer(Array(try istream.popBytes(4).reversed())).load(as: Float32.self))
+		case 0xfb:
+            return CBOR.double(UnsafeRawPointer(Array(try istream.popBytes(8).reversed())).load(as: Float64.self))
 
-		case 0xff: return CBOR.Break
+		case 0xff: return CBOR.break
 		default: return nil
 		}
 	}
