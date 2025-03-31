ff3143f87c70ad6f8604a03f355776d0fe782378
Marc Prud'hommeaux <marc@skip.tools>
2025-03-31 09:14:53 -0400

    Add spcial-case measure function for Android to work around large standard deviation on emulator (https://github.com/swiftlang/swift-corelibs-xctest/pull/506)

--- Tests/CBORDecoderTests.swift
+++ Tests/CBORDecoderTests.swift
@@ -202,3 +202,20 @@ class CBORDecoderTests: XCTestCase {
         }
     }
 }
+
+#if os(Android)
+
+extension XCTestCase {
+    /// XCTestCase.measure on Android is problematic because
+    /// the emulator on a virtualized runner can be quite slow
+    /// but there is no way to set the standard deviation threshold
+    /// for failure, so we override it to simply run the block
+    /// and not perform any measurement.
+    /// 
+    /// See: https://github.com/swiftlang/swift-corelibs-xctest/pull/506
+    func measure(_ count: Int = 0, _ block: () -> ()) {
+        block()
+    }
+}
+#endif
+
